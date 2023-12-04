8a198375d5db49ae5f34923f56ba766197ec4e5b
Andersson <anderssonbustos@gmail.com>
2023-12-04 00:10:43 -0500

    Cambios codigo sql en archivo database.sql

--- Database/database.sql
+++ Database/database.sql
@@ -56,14 +56,10 @@ VALUES
     ('rana', 1, 4, 4),
     ('escalera', 3, 11, 8),
     ('camión', 2, 9, 6),
-    ('naranja', 3, 13, 7);
-
--- Insertar palabras en el banco_palabras con tamaños de 9, 10 y 11
-INSERT INTO banco_palabras (bapa_palabra, bapa_nivel, bapa_puntos, bapa_tamanopalabra)
-VALUES
-    ('mariposa', 3, 15, 9),    -- Nivel 3, 15 puntos, tamaño 9
-    ('aventura', 3, 16, 10),   -- Nivel 3, 16 puntos, tamaño 10
-    ('elefante', 3, 18, 11),   -- Nivel 3, 18 puntos, tamaño 11
-    ('camiseta', 2, 10, 9),    -- Nivel 2, 10 puntos, tamaño 9
-    ('computadora', 3, 17, 11), -- Nivel 3, 17 puntos, tamaño 11
-    ('caballero', 2, 12, 10);   -- Nivel 2, 12 puntos, tamaño 10
+    ('naranja', 3, 13, 7),
+    ('mariposa', 3, 15, 9),    
+    ('aventura', 3, 16, 10),   
+    ('elefante', 3, 18, 11),   
+    ('camiseta', 2, 10, 9),    
+    ('computadora', 3, 17, 11), 
+    ('caballero', 2, 12, 10);   
